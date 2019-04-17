d7cb3826d79a0ad9458544656b38dbb28f74adf1
shenjian <shenjian5@xiaomi.com>
2019-04-17 22:50:30 +0800

    modi

--- lib/expansion_listview.dart
+++ lib/expansion_listview.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 
 class _ShjKey<S, V> extends LocalKey {
   const _ShjKey(this.shj, this.value);
-
+  
   final S shj;
   final V value;
-
+  
   @override
   bool operator ==(dynamic other) {
     if (other.runtimeType != runtimeType) return false;
     final _ShjKey<S, V> typedOther = other;
     return shj == typedOther.shj && value == typedOther.value;
   }
-
+  
   @override
   int get hashCode => hashValues(runtimeType, shj, value);
-
+  
   @override
   String toString() {
     final String saltString = S == String ? '<\'$shj\'>' : '<$shj>';
@@ -24,7 +24,7 @@ class _ShjKey<S, V> extends LocalKey {
   }
 }
 
-typedef ExpansionCellCallBack = void Function(int cellIndex, bool isExpanded);
+typedef ExpansionCellCallBack = void Function(int cellIndex);
 
 typedef ExpansionCellHeader = Widget Function(BuildContext context, bool isExpanded);
 
@@ -32,30 +32,30 @@ class ExpansionCell {
   final ExpansionCellHeader cellHeader;
   final Widget body;
   final bool isExpanded;
-
+  
   ExpansionCell({
     @required this.cellHeader,
     @required this.body,
     this.isExpanded = false,
   })  : assert(cellHeader != null),
-        assert(body != null),
-        assert(isExpanded != null);
+            assert(body != null),
+            assert(isExpanded != null);
 }
 
 class ExpansionListView extends StatefulWidget {
   final List<ExpansionCell> children;
   final ExpansionCellCallBack expansionCellCallBack;
   final Duration animationDuration;
-
+  
   const ExpansionListView({
     Key key,
     this.children = const <ExpansionCell>[],
     this.expansionCellCallBack,
     this.animationDuration = kThemeAnimationDuration,
   })  : assert(children != null),
-        assert(animationDuration != null),
-        super(key: key);
-
+            assert(animationDuration != null),
+            super(key: key);
+  
   @override
   _ExpansionListViewState createState() => _ExpansionListViewState();
 }
@@ -65,33 +65,33 @@ class _ExpansionListViewState extends State<ExpansionListView> {
   void initState() {
     super.initState();
   }
-
+  
   bool _isChildExpanded(int index) {
     return widget.children[index].isExpanded;
   }
-
-  void _handlePressed(bool isExpanded, int index) {
-    if (widget.expansionCellCallBack != null) widget.expansionCellCallBack(index, isExpanded);
+  
+  void _handlePressed(int index) {
+    if (widget.expansionCellCallBack != null) widget.expansionCellCallBack(index);
     setState(() {});
   }
-
+  
   @override
   Widget build(BuildContext context) {
     final List<MergeableMaterialItem> items = <MergeableMaterialItem>[];
     for (int index = 0; index < widget.children.length; index += 1) {
-//      if (_isChildExpanded(index) && index != 0 && !_isChildExpanded(index - 1))
-//        items.add(MaterialGap(key: _ShjKey<BuildContext, int>(context, index * 2 - 1)));
-
       final ExpansionCell child = widget.children[index];
-      final Row header = Row(
+      
+      final Stack shjHeader = Stack(
         children: <Widget>[
-          Expanded(
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.yellow,
+            ),
             child: AnimatedContainer(
               duration: widget.animationDuration,
               curve: Curves.fastOutSlowIn,
               margin: EdgeInsets.zero,
-              child: ConstrainedBox(
-                constraints: const BoxConstraints(minHeight: 100.0),
+              child: Container(
                 child: child.cellHeader(
                   context,
                   _isChildExpanded(index),
@@ -99,25 +99,25 @@ class _ExpansionListViewState extends State<ExpansionListView> {
               ),
             ),
           ),
-          Container(
-            margin: const EdgeInsetsDirectional.only(end: 8.0),
-            child: ExpandIcon(
-              isExpanded: _isChildExpanded(index),
-              padding: const EdgeInsets.all(16.0),
-              onPressed: (bool isExpanded) {
-                return _handlePressed(isExpanded, index);
-              },
+          InkWell(
+            onTap: () {
+              print('sssssss');
+              return _handlePressed(index);
+            },
+            child: SizedBox(
+              width: double.maxFinite,
+              height: 50,
             ),
           ),
         ],
       );
-
+      
       items.add(
         MaterialSlice(
           key: _ShjKey<BuildContext, int>(context, index * 2),
           child: Column(
             children: <Widget>[
-              MergeSemantics(child: header),
+              MergeSemantics(child: shjHeader),
               AnimatedCrossFade(
                 firstChild: Container(height: 0.0),
                 secondChild: child.body,
@@ -125,21 +125,19 @@ class _ExpansionListViewState extends State<ExpansionListView> {
                 secondCurve: const Interval(0.4, 1.0, curve: Curves.fastOutSlowIn),
                 sizeCurve: Curves.fastOutSlowIn,
                 crossFadeState:
-                    _isChildExpanded(index) ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+                _isChildExpanded(index) ? CrossFadeState.showSecond : CrossFadeState.showFirst,
                 duration: widget.animationDuration,
               )
             ],
           ),
         ),
       );
-
-//			if (_isChildExpanded(index) && index != widget.children.length - 1)
-//				items.add(MaterialGap(key: _ShjKey<BuildContext, int>(context, index * 2 + 1)));
     }
-
+    
     return MergeableMaterial(
-			hasDividers: true,
-			children: items,
-		);
+      hasDividers: false,
+      children: items,
+    );
   }
 }
+
